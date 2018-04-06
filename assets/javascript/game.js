5b3c612ac53077c2df9bfc49c5559b019040352b
Andrew M. Lilly <amlilly@Andrews-MacBook-Pro.local>
2018-04-05 23:55:35 -0500

    Linked associated files, prepared to use Bootstrap and  cleaned up psuedo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// GLOBAL VARIABLES
+// --------------------------------------------------------------------
+
+// A random number will need to be generated for the user to target
+    // Math.random()
+
+// The Wins and Loss of all the completed attempts made by user
+
+// Click event oon the crystal images
+
+// Display the user's current total score
